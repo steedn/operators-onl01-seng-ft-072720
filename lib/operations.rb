fd8101fbb954e27b2c306efaa8768b8fc0996618
Nathan Steed <steedn@outlook.com>
2020-07-27 23:23:55 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,5 +12,5 @@ end
 def not_safe?(speed)
 	speed < 40 || speed > 60 ? true : false
 end
-# originally had speed < 40 ? true : speed > 60 ? true : false   
+# originally had speed < 40 ? true : speed > 60 ? true : false
 # however I thought that the one i have currently looks better
